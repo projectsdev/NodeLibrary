76153a5766b4c93fc71c429a42f24295e2b197bc
Akhil <akhilrk19@gmail.com>
2018-11-02 11:51:24 +0530

    align

--- api/controllers/main_function.js
+++ api/controllers/main_function.js
@@ -1,15 +1,5 @@
 'use strict';
-/*
- 'use strict' is not required but helpful for turning syntactical errors into true errors in the program flow
- https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Strict_mode
-*/
 
-/*
- Modules make it possible to import JavaScript files into your application.  Modules are imported
- using 'require' statements that give you a reference to the module.
-
-  It is a good idea to list the modules that your application depends on in the package.json in the project root
- */
 var util = require('util');
 var admin = require("firebase-admin");
 var mysql = require('mysql');
